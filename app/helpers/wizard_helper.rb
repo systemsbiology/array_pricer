1c6c0180629b14dee085742ed1f3f417ac54cbf9
Bruz Marzolf <bmarzolf@systemsbiology.org>
2009-05-15 08:50:12 -0700

    Show price per slide when there are multiple arrays per slide.

--- app/helpers/wizard_helper.rb
+++ app/helpers/wizard_helper.rb
@@ -6,4 +6,13 @@ module WizardHelper
       number_to_currency(price)
     end
   end
+
+  def cost_per_slide(array_price, number)
+    if(number > 1 && array_price)
+      formatted_slide_price = number_to_currency(array_price*number)
+      " (#{formatted_slide_price} per slide / #{number} arrays)"
+    else
+      ""
+    end
+  end
 end
